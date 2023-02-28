e094e466e611de4cce93835326563c15d24895b9
Daniel P. López <danielpl@iptp.pe>
2023-02-27 23:45:06 -0500

    Added Employees model

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class Employee(models.Model):
+	first_name = models.CharField(max_length=60)
+	last_name = models.CharField(max_length=60)
+	dni = models.CharField(max_length=8)
+	area = models.CharField(max_length=30)
+	active = models.BooleanField(default=True)
+	salary = models.IntegerField()
+	contract_type = models.CharField(max_length=20)
+	full_time = models.BooleanField(default=True)
+
+
+admin.site.register(Employee)
