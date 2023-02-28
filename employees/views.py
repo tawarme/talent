180607b35890acd0f693e8ba64cdf42030db9366
Daniel P. López <danielpl@iptp.pe>
2023-02-28 13:23:41 -0500

    Implemented Employees GET

--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.http import Http404
+
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from employees.models import Employee
+from employees.serializers import EmployeeSerializer
 
 
 class EmployeeView(APIView):
-	def get(self, request):
-		pass
+	def get_employee(self, pk):
+		try:
+			return Employee.objects.get(pk=pk)
+		except:
+			raise Http404
+
+	def get(self, request, pk):
+		employee = self.get_employee(pk)
+		serializer = EmployeeSerializer(employee)
+
+		return Response(serializer.data)
 
 	def update(self, request):
 		pass
