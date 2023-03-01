2e6c7da3f157b682ad1f517f2cf0f6392a6deced
Daniel P. López <danielpl@iptp.pe>
2023-03-01 11:28:34 -0500

    Added Choices to contract type

--- employees/models.py
+++ employees/models.py
@@ -3,13 +3,16 @@ from django.contrib import admin
 
 
 class Employee(models.Model):
+	CONTRACT_TYPES = [("contractor", "Contractor"),
+					  ("planilla", "Planilla")]
+
 	first_name = models.CharField(max_length=60)
 	last_name = models.CharField(max_length=60)
 	dni = models.CharField(max_length=8, unique=True)
 	area = models.CharField(max_length=30)
 	active = models.BooleanField(default=True)
 	salary = models.IntegerField()
-	contract_type = models.CharField(max_length=20)
+	contract_type = models.CharField(max_length=20, choices=CONTRACT_TYPES)
 	full_time = models.BooleanField(default=True)
 
 
