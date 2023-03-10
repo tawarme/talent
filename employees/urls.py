54c2664317e75500cf0f79f9cf24bd4e295a5105
Daniel P. López <danielpl@iptp.pe>
2023-03-10 12:57:01 -0500

    Employee viewset added

--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from employees.views import EmployeeView, EmployeeCreateView
+from rest_framework.routers import DefaultRouter
+#from employees.views import EmployeeView, EmployeeCreateView
+from employees.views import EmployeeViewSet
 
-urlpatterns = [
-    path('employees/', EmployeeCreateView.as_view()),
-    path('employees/<int:pk>/', EmployeeView.as_view()),
-]
+
+router = DefaultRouter()
+router.register('employees', EmployeeViewSet, basename='employee')
+
+urlpatterns = router.urls
