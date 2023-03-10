2a608814deaf1ce5615f23264110f1d2976d3907
Daniel P. López <danielpl@iptp.pe>
2023-03-10 12:57:01 -0500

    Updated .gitignore

--- employees/views.py
+++ employees/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.hashers import check_password, make_password
 from django.contrib.auth.password_validation import validate_password
 
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -52,6 +53,11 @@ class EmployeeCreateView(APIView):
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+	serializer_class = EmployeeSerializer
+	queryset = Employee.objects.all()
 
 
 class UserChangePassView(APIView):
