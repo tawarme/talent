a3515e99c7ce29561c1b338374cc7d69e551483b
Daniel P. López <danielpl@iptp.pe>
2023-03-17 00:07:38 -0500

    Added current assignation nested in employee endpoint

--- employees/serializers.py
+++ employees/serializers.py
@@ -2,7 +2,15 @@ from rest_framework.serializers import Serializer, ModelSerializer, CharField
 from employees.models import Employee, Assignation, EmployeeIncidents
 
 
+class AssignationSerializer(ModelSerializer):
+	class Meta:
+		model = Assignation
+		fields = '__all__'
+
+
 class EmployeeSerializer(ModelSerializer):
+	current_assignation = AssignationSerializer(read_only=True)
+
 	class Meta:
 		model = Employee
 		fields = '__all__'
@@ -17,9 +25,3 @@ class EmployeeIncidentSerializer(ModelSerializer):
 class PasswordChangeSerializer(Serializer):
 	current_password = CharField()
 	new_password = CharField()
-
-
-class AssignationSerializer(ModelSerializer):
-	class Meta:
-		model = Assignation
-		fields = '__all__'
